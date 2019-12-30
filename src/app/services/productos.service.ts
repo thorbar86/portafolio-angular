1f563f1656d7aaff84e4f4e871c1214cb7aeeefa
David Ribe <david.ribe@uvic.cat>
2019-12-30 22:59:09 +0100

    finish the search button

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -2,27 +2,81 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
 
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProductosService {
 
   cargando = true;
+
   productos: Producto[] = [];
 
+  productosFiltrados: Producto [] = [];
+
   constructor(private http: HttpClient) {
+
     this.cargarProductos();
   }
 
   private cargarProductos() {
+
+
+    return new Promise ( ( resolve, reject) => {
+
     this.http.get('https://angular-html-cb07e.firebaseio.com/productos_idx.json')
       .subscribe( (resp: Producto[]) => {
         this.productos = resp;
         this.cargando = false;
+        resolve();
       });
+
+    });
+
   }
   getProducto( id: string ) {
     return this.http.get(`https://angular-html-cb07e.firebaseio.com/productos/${ id }.json`);
 
   }
+  
+
+
+  buscarProducto ( termino: string ) {
+  
+
+    if (this.productos.length == 0 ) {
+       /* cargamos productos*/
+      this.cargarProductos().then( ()=> {
+      /*ejecucatmos despues de tner productos */
+
+      this.filtrarProductos ( termino );
+      });
+    }else {
+
+      this.filtrarProductos ( termino );
+
+      
+    }
+       }
+
+
+
+    private filtrarProductos( termino: string) {
+
+      console.log (this.producto);
+      this.productosFiltrado = [];
+
+      termino = termino.toLocaleLowerCase();
+  
+      this.productos.forEach( prod => {
+        
+        const tituloLower = prod.titulo.toLocaleLowerCase();
+
+        if (prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf ( termino ) >= 0 ) {
+
+          this.productosFiltrado.push( prod );
+        }
+      });
+    }
 }
